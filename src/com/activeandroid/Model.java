6a137b447173707b629a8019ef02c2f7b86aa4bd
hector <hectorups@gmail.com>
2014-06-08 13:41:00 -0700

    allow to set the id of the model or Parcelable can't be used with activeandroid

--- src/com/activeandroid/Model.java
+++ src/com/activeandroid/Model.java
@@ -63,6 +63,10 @@ public abstract class Model {
 		return mId;
 	}
 
+    public final void setId(Long id){
+        mId = id;
+    }
+
 	public final void delete() {
 		Cache.openDatabase().delete(mTableInfo.getTableName(), idName+"=?", new String[] { getId().toString() });
 		Cache.removeEntity(this);
